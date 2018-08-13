22f1641264b2b3ecf0d577d10d605172ada1359e
renbingdong <renbingdong@yongche.com>
2018-08-13 14:12:57 +0800

    update

--- mvc/view/index/welcome.php
+++ mvc/view/index/welcome.php
@@ -2,9 +2,12 @@
     <head>
         <title>欢迎来到我的世界</title>
         <link rel="stylesheet" href="/static/css/bootstrap.min.css">
+        <link rel="shortcut icon" href="/static/pic/icon.ico" type="image/x-icon" >
 
         <style type="text/css">
             .hugescreen {
+                height: 100%;
+                margin: 0px;
                 background-image: url(/static/pic/hugescreen.png);
             }
         </style>
