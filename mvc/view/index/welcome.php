71722054c30148b7fd8ba1bbca536c3d73c7ed43
renbingdong <renbingdong@yongche.com>
2018-08-13 11:23:15 +0800

    update

--- mvc/view/index/welcome.php
+++ mvc/view/index/welcome.php
@@ -1,9 +1,11 @@
 <html>
     <head>
         <title>欢迎来到我的世界</title>
+        <link rel="stylesheet" href="/static/css/bootstrap.min.css">
     </head>
     <body>
-        <?php echo "hello {$name}"; ?>
-    
+        <div class="jumbotron">
+            <h1>hello, world!</h1>
+        </div>
     </body>
 </html>
