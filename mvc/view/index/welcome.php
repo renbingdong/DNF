990767ca5d2202379decd70b0536f25f72709f74
renbingdong <renbingdong@yongche.com>
2018-08-13 11:59:28 +0800

    update

--- mvc/view/index/welcome.php
+++ mvc/view/index/welcome.php
@@ -2,9 +2,15 @@
     <head>
         <title>欢迎来到我的世界</title>
         <link rel="stylesheet" href="/static/css/bootstrap.min.css">
+
+        <style type="text/css">
+            .hugescreen {
+                background-image: url(/static/pic/hugescreen.png);
+            }
+        </style>
     </head>
     <body>
-        <div class="jumbotron">
+        <div class="jumbotron hugescreen">
             <h1>hello, world!</h1>
         </div>
     </body>
